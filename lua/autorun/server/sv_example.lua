f7552bf94289ce8e49ce7f5a0316d525418285cc
Jack Jenkins <jack.jenkins2@yahoo.com>
2017-09-27 17:47:08 +0100

    new crash method lol

--- lua/autorun/server/sv_example.lua
+++ lua/autorun/server/sv_example.lua
@@ -1,8 +1,8 @@
 -- This file should be in lua/autorun/server/sv_whatever.lua
 
 concommand.Add( "_exit", function( ply )
-	hook.Run("ShutDown") -- Call shutdown so MySQL stuff can save
+	hook.Run("ShutDown") -- Call this to get MySQL and stuff to save
 	print("Server shutting down...")
 
-	Entity(0):Remove() -- Kill server
+	Entity(0):GetAnimInfo(9999999999) -- Crashes the server to trigger reboot
 end )
