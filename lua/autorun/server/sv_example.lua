404792f4d21b1528814503e9e4afec2a8ce7e393
Jack Jenkins <jack.jenkins2@yahoo.com>
2017-09-27 18:13:42 +0100

    crash redundancy lol

--- lua/autorun/server/sv_example.lua
+++ lua/autorun/server/sv_example.lua
@@ -4,5 +4,7 @@ concommand.Add( "_exit", function( ply )
 	hook.Run("ShutDown") -- Call this to get MySQL and stuff to save
 	print("Server shutting down...")
 
-	Entity(0):GetAnimInfo(9999999999) -- Crashes the server to trigger reboot
+	Entity(0):GetAnimInfo(99999999) -- Crashes the server to trigger reboot
+	Entity(0):Input()
+	Entity(0):Remove() -- They fixed this wat?
 end )
